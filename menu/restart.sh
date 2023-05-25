39a78c4c30a3f64782877b5374a2c55d6c60b7a2
mousethain <mousethain@gmail.com>
2023-05-25 18:49:13 +0700

    Update restart.sh

--- menu/restart.sh
+++ menu/restart.sh
@@ -2,7 +2,7 @@
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 CEKEXPIRED () {
-    today=$(date -d +1day +%Y-%m-%d)
+    today=$(date +d +1day +%Y-%m-%d)
     Exp1=$(curl -sS https://raw.githubusercontent.com/nmousethain/cvk/mouse/permission/ip | grep $MYIP | awk '{print $3}')
     if [[ $today > $Exp1 ]]; then
     echo -e "\e[32mSTATUS SCRIPT AKTIF...\e[0m"
