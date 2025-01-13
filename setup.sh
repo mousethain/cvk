b453827230c0946f4cd54665f62f8c22e3f7b7bd
mousethain <mousethain@gmail.com>
2025-01-13 18:37:27 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,10 +30,10 @@ Bloman () {
 if [ -f "/etc/.$Name.ini" ]; then
 CekTwo=$(cat /etc/.$Name.ini)
     if [ "$CekOne" = "$CekTwo" ]; then
-        res="Expired"
+        res="Permission Accepted"
     fi
 else
-res="Permission Accepted..."
+res="Expired..."
 fi
 }
 
